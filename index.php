92ca49bf6a756afcbf6270283cb983ac8a1ccf75
AlessandroDiNardo <alexdna003@gmail.com>
2023-01-11 17:52:43 +0100

    change layout

--- index.php
+++ index.php
@@ -4,9 +4,58 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <script src="https://kit.fontawesome.com/897e5eb927.js" crossorigin="anonymous"></script>
+    <title>Strong Password Generator</title>
 </head>
 <style>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body{
+        background-color: #3ba42c   ;
+        font-family: arial, sans-serif;
+    }
+
+    .container{
+        max-width: 1000px;
+        margin: 0 auto;
+        text-align: center;
+        padding: 200px 0;
+        line-height: 50px;
+    }
+
+    form{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 20px;
+    }
+
+    .box-num{
+        padding: 10px 25px;
+        text-align: center;
+        font-size: 12px;
+        border: 1px solid white;
+        border-radius: 5px;
+    }
+
+    .btn-gen{
+        border: 1px solid white;
+        border-radius: 5px;
+        padding: 5px 15px;
+        background-color: white;
+        color: #3ba42c;
+    }
+
+    .btn-gen:hover{
+        cursor: pointer;
+        background-color: #3ba42c;
+        color: white;
+        border: 1px solid black;
+    }
 
 </style>
 <?php
@@ -16,22 +65,26 @@
     require_once __DIR__ . "/helper.php";
 ?>
 <body>
-    <h1>Strong Password Generator</h1>
-    <form method="GET">
-        <label for="numchar">Lunghezza Password:</label>
-        <input type="text" name="numchar">
-        <input type="submit" value="GENERATE">
-    </form>
+    <div class="container">
+        <h1>Strong Password Generator</h1>
+            <form method="GET">
+                <label for="numchar">Choose password length:</label>
+                <input type="text" name="numchar" placeholder="min 8 chars..." class="box-num">
+                <button value="GENERATE" class="btn-gen">
+                    <i class="fa-solid fa-paper-plane"></i>
+                </button>
+            </form>
 
-    <?php
-        $newPsw = randomPassword($numChar);
+            <?php
+                $newPsw = validatePassword($numChar);
 
-        if($newPsw){
+                if($newPsw){
 
-            $_SESSION["newpsw"] = $newPsw;
-            header('Location: ./psw.php');
-        }
+                    $_SESSION["newpsw"] = $newPsw;
+                    header('Location: ./psw.php');
+                }
 
-    ?>
+            ?>
+    </div>
 </body>
 </html>
--- psw.php
+++ psw.php
@@ -6,17 +6,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>New Password</title>
 </head>
+<style>
+     *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    body{
+        background-color: #3ba42c   ;
+        font-family: arial, sans-serif;
+    }
+
+    .container{
+        max-width: 1000px;
+        margin: 0 auto;
+        text-align: center;
+        padding: 200px 0;
+        line-height: 50px;
+    }
+</style>
 <?php
     session_start();
 
     $newPsw = $_SESSION["newpsw"];
 ?>
 <body>
-    <?php
-        echo "<div> La tua nuova password è:" 
-        . "<h1>" . $newPsw . "</h1>" 
-        . "</div>";
-        echo "<a href='./index.php'>Return to the main page</a>";
-    ?>
+    <div class="container">
+        <?php
+                echo "<div> La tua nuova password è:" 
+                . "<h1>" . $newPsw . "</h1>" 
+                . "</div>";
+                echo "<a href='./index.php'>Return to the main page</a>";
+            ?>
+    </div>
 </body>
 </html>
